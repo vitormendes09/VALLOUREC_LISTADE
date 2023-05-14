83bdd95cab9080f2dd1c606f609bd700dc4dfa0f
Vitor Silva Mendes <93722266+vitormendes09@users.noreply.github.com>
2023-05-14 16:24:20 -0300

    Ex3
    Resolvido o terceiro exercício de concatenação de listas

--- Ex3.java
+++ Ex3.java
@@ -1,3 +1,45 @@
 public class Ex3 {
+    public class Exemplo {
+
+        public static void main(String[] args) {
+            int[] x = {-1, 10, 8};
+            int[] y = {3, 6, 15};
+            int[] z = combinarListas(x, y);
+        }
+
+    }
+
+    public static int[] combinarListas(int[] x, int[] y) {
+        int[] z = new int[x.length + y.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while (i < x.length && j < y.length) {
+            if (x[i] <= y[j]) {
+                z[k] = x[i];
+                i++;
+            } else {
+                z[k] = y[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < x.length) {
+            z[k] = x[i];
+            i++;
+            k++;
+        }
+        
+        while (j < y.length) {
+            z[k] = y[j];
+            j++;
+            k++;
+        }
+        
+        return z;
+    }
+    
     
 }
